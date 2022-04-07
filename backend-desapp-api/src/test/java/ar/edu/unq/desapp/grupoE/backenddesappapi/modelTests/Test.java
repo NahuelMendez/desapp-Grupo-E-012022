73a24f674b2effd1d3ee2f3ac5980b9a03744093
Nahuel Mendez <mendez.nahuel@outlook.com>
2022-04-06 23:35:58 -0300

    Commit inicial - primer test

---
+++ backend-desapp-api/src/test/java/ar/edu/unq/desapp/grupoE/backenddesappapi/modelTests/Test.java
@@ -0,0 +1,12 @@
+package ar.edu.unq.desapp.grupoE.backenddesappapi.modelTests;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ModelTest {
+
+    @Test
+    void testInicial() {
+        assertEquals(true, true);
+    }
+}
