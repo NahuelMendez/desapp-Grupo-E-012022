6f30891e6f0f8a8aa18569eac59d5908759f32c6
ximena-bogado <ximenakbq@gmail.com>
2022-04-10 19:57:21 -0300

    reestructuracion del proyecto

---
+++ src/test/java/ar/edu/unq/desapp/grupoE/backenddesappapi/modelTests/ModelTest.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.grupoE.backenddesappapi.modelTests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ModelTest {
+
+    @Test
+    void testInicial() {
+        assertEquals(true, true);
+    }
+}
