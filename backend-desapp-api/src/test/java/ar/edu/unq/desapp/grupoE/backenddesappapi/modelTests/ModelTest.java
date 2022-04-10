dccbc318987274bcf81e4efed10f3c1f969e7477
ximena-bogado <ximenakbq@gmail.com>
2022-04-10 15:15:29 -0300

    Test CI

--- backend-desapp-api/src/test/java/ar/edu/unq/desapp/grupoE/backenddesappapi/modelTests/ModelTest.java
+++ backend-desapp-api/src/test/java/ar/edu/unq/desapp/grupoE/backenddesappapi/modelTests/ModelTest.java
@@ -1,12 +1,13 @@
 package ar.edu.unq.desapp.grupoE.backenddesappapi.modelTests;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class ModelTest {
+import static org.junit.Assert.assertEquals;
+
+public class ModelTest {
 
     @Test
     void testInicial() {
-        assertEquals(true, true);
+        assertEquals(true, false);
     }
 }
