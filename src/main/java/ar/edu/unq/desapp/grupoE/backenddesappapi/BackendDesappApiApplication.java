397e4a79d716aa1ec3f905b5d421c3978a819aa7
ximena-bogado <ximenakbq@gmail.com>
2022-04-10 22:38:12 -0300

    hello world

--- src/main/java/ar/edu/unq/desapp/grupoE/backenddesappapi/BackendDesappApiApplication.java
+++ src/main/java/ar/edu/unq/desapp/grupoE/backenddesappapi/BackendDesappApiApplication.java
@@ -2,10 +2,19 @@ package ar.edu.unq.desapp.grupoE.backenddesappapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class BackendDesappApiApplication {
 
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(BackendDesappApiApplication.class, args);
 	}
