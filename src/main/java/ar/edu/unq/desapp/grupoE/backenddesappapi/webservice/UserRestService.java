2c85c233ac0f747494cc6508405efb93e090b98e
ximena-bogado <ximenakbq@gmail.com>
2022-05-01 21:43:45 -0300

    config swagger

--- src/main/java/ar/edu/unq/desapp/grupoE/backenddesappapi/webservice/UserRestService.java
+++ src/main/java/ar/edu/unq/desapp/grupoE/backenddesappapi/webservice/UserRestService.java
@@ -8,10 +8,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
