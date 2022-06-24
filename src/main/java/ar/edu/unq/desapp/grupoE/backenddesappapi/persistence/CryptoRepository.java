26b8264b566b89242382bbbb906be9c00211efeb
ximena-bogado <ximenakbq@gmail.com>
2022-06-23 22:55:13 -0300

    obtener las cotizaciones de las ultimas 24 horas

--- src/main/java/ar/edu/unq/desapp/grupoE/backenddesappapi/persistence/CryptoRepository.java
+++ src/main/java/ar/edu/unq/desapp/grupoE/backenddesappapi/persistence/CryptoRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Configuration
@@ -18,4 +19,10 @@ public interface CryptoRepository extends CrudRepository<CryptoQuote, Integer> {
                                     "FROM CryptoQuote quote " +
                                     "GROUP BY quote.name)")
     List<CryptoQuote> findCryptoQuoteLastUpdate();
+
+    @Query( "DELETE FROM CryptoQuote quote WHERE quote.updateTime < ?1")
+    void deleteCryptoQuotePast24hr(LocalDateTime date);
+
+    @Query( "SELECT quote FROM CryptoQuote quote WHERE quote.name = ?1")
+    List<CryptoQuote> findAllCryptoQuoteFor(String symbol);
 }
--- src/main/java/ar/edu/unq/desapp/grupoE/backenddesappapi/service/CryptoQuoteService.java
+++ src/main/java/ar/edu/unq/desapp/grupoE/backenddesappapi/service/CryptoQuoteService.java
@@ -9,6 +9,9 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -24,6 +27,14 @@ public class CryptoQuoteService {
         return this.cryptoQuoteProvider.getCryptoQuoteBySymbol(symbol);
     }
 
+    public List<CryptoQuote> getAllCryptoQuoteFor(String symbol){
+        List<CryptoQuote> quotes = cryptoRepository.findAllCryptoQuoteFor(symbol);
+        if (quotes.isEmpty()) {
+            return Collections.singletonList(this.cryptoQuoteProvider.getCryptoQuoteBySymbol(symbol));
+        }
+        return quotes;
+    }
+
     public List<CryptoQuote> getAllCryptoQuotes(){
         return cryptoRepository.findCryptoQuoteLastUpdate();
     }
@@ -32,5 +43,11 @@ public class CryptoQuoteService {
     @Scheduled(fixedDelay = 600000)
     public void saveAllCryptoQuotes() {
         cryptoRepository.saveAll(this.cryptoQuoteProvider.getAllCryptoQuotes());
+    }
+
+    @Async
+    @Scheduled(cron = "0 0 */1 * * *")
+    public void deleteCryptoQuotePast24hr() {
+        cryptoRepository.deleteCryptoQuotePast24hr(LocalDateTime.now().minusHours(24));
     }
 }
